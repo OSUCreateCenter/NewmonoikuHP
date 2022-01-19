ddfb20f87b7cfa29c534023d921369e20a64dab2
createstaff <staff@createstaffnoiMac.local>
2022-01-19 10:03:39 +0900

    backp

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// TextDecoderStream
+$(function() {
+    $('.slider').slick({
+        autoplay: true, // 自動再生ON
+        dots: true, // ドットインジケーターON
+        centerMode: true, // 両サイドに前後のスライド表示
+        centerPadding: '0px', // 左右のスライドのpadding
+        slidesToShow: 3, // 一度に表示するスライド数
+    });
+});
